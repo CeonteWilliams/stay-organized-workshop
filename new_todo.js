1c72a4160f2e6d74c3021c464102d1ea1fb5e803
becky-bio <beckybio07@gmail.com>
2023-12-08 11:30:19 -0500

    Populate categories for new task

--- new_todo.js
+++ new_todo.js
@@ -1,3 +1,22 @@
+//fetching usernames from the API by calling api/users
+
+fetch("http://localhost:8083/api/users")
+    .then(response => response.json())
+    .then(users => {
+        const userDropdown = document.getElementById('user');
+
+        //creating the users dropdown
+
+        users.forEach(user => {
+            const option = document.createElement('option');
+            option.value = user.id;
+            option.text = user.name;
+            userDropdown.appendChild(option);
+        });
+    })
+    .catch(error => console.error('Error fetching users:', error));
+
+
 //fetching category names from the API by calling api/categories
 
 fetch("http://localhost:8083/api/categories")
@@ -19,4 +38,6 @@ fetch("http://localhost:8083/api/categories")
 
 
 
-    
+
+
+
